cde2d4979d4e45c6018717332ff5946bb4b29b6b
Michael Simard <mike.freelancer@gmail.com>
2023-10-06 11:07:20 -0400

    Update main.py

--- main.py
+++ main.py
@@ -46,6 +46,7 @@ def show_info():
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name='full_url" content='""" + request.url +  """'> 
         
         <title>Header, Cookies, and Window Location Details</title>
         <!-- for Atlas Affiliate -->
