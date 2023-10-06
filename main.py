f39b3f8759be425272704e6e4a2703879e7d8e56
Michael Simard <mike.freelancer@gmail.com>
2023-10-06 10:22:51 -0400

    Update main.py

--- main.py
+++ main.py
@@ -52,7 +52,7 @@ def show_info():
         <script type="text/javascript" src='//cdn.adultforce.com/atlas/atlaslib.min.js'></script>
         <script type="text/javascript" src='//cdn.adultforce.com/vortex/vortex.modern.min.js'></script>
         <script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
-        <script type="text/javascript" src='https://storage.googleapis.com/looker_vis/debug_external.js?v=5'></script>
+        <script type="text/javascript" src='https://storage.googleapis.com/looker_vis/debug_external.js?v=8'></script>
         <script type="text/javascript">
 
         $(document).ready(function(){
